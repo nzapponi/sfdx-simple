c759d3041a59abf8e45a54c9e6b5306185677548
Niccolò Zapponi <nzapponi@salesforce.com>
2017-12-01 09:30:26 +0000

    Added sample data

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,4 @@
 sfdx force:org:create -f config/project-scratch-def.json -s -a vol -d 1
 sfdx force:source:push
-sfdx force:user:permset:assign -n TheWizardApp
 sfdx force:data:tree:import -p data/masterImportPlan.json
 sfdx force:org:open
